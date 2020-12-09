41f1809b793831df2872db747497383e99e6bc80
Ultra-Code <mega.alpha100@gmail.com>
2020-12-09 23:24:46 +0000

    Add library support for value based exceptions using boost outcome
    Define a custom domain for my value based exceptions
    
    And another custom domain using quick_status_code provided by outcome

---
+++ libcpputils/value_based_exceptions/main.outcome.cxx
@@ -0,0 +1,26 @@
+#include <fmt/printf.h>
+#include <libcpputils/value_based_exceptions/outcome.generic.hxx>
+
+[[noreturn]] auto
+trys()
+{
+    fmt::print("I am trys\n");
+    throw domain::my_generic_status_error_(domain::my_status::failure);
+}
+
+auto
+main_outcome() -> void
+{
+    try
+    {
+        fmt::print("I am hear try\n");
+        trys();
+    }
+    catch (domain::my_generic_status_error_ static_exceptions)
+    {
+        fmt::print("I am hear catch\n");
+        fmt::print("Domain {} with message {} \ncode {} \n",
+                   static_exceptions.code().domain().name().c_str(),
+                   static_exceptions.what(), static_exceptions.code().value());
+    }
+}
